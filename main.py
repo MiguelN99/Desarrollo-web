72ec234b1c17ad362061e0fbb03be6616603ce7d
MiguelN99 <Miguelne1999@gmail.com>
2022-11-16 21:28:55 -0500

    API Basica

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"Hola": "Mundo"}
+@app.get("/saludo")
+def root():
+    return {
+        
+        "Hola": "Mision Tic"
+        
+    }
+
+@app.get("/usuarios/{user_id}")
+def read_user(user_id: int):
+    return {
+        
+        "user_id": user_id
+        
+    }
+
+
+cursos = [
+
+    {"cursos": "Fundamentos de programacion"},
+    {"cursos": "Programacion Basica"},
+    {"cursos": "Desarrollo de Software"},
+    {"cursos": "Desarrollo Web"},
+
+]
+
+@app.get("/cursos")
+def read_item(skip: int = 0, limit: int = 10):
+    return cursos[skip: skip + limit]          
